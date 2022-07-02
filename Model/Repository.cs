5fe207a20e32d2743b35afc136d391d2c3f0bf52
Kungfoowiz <kungfoowiz@hotmail.com>
2022-07-02 12:01:45 +0100

    Create Repository.cs

---
+++ Model/Repository.cs
@@ -0,0 +1,25 @@
+// Model for JSON result.
+
+using System;
+
+using System.Text.Json.Serialization;
+
+namespace Model.WebAPIClient
+{
+    public class Repository
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+        [JsonPropertyName("watchers")]
+        public int Watchers { get; set; }
+
+        [JsonPropertyName("pushed_at")]
+        public DateTime LastPushUtc { get; set; }
+
+        public DateTime LastPush => LastPushUtc.ToLocalTime();
+    }
+}
